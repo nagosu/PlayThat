850510550cda69b06ced6b1163db91262a64c206
nagosu <nozick1021@gmail.com>
2024-07-14 02:24:18 +0900

    모달 작업

---
+++ static/chatbot_v1_pt/js/modal.js
@@ -0,0 +1,16 @@
+// 종료하기 버튼 클릭 시 모달 띄우기
+document.querySelector(".exit-button").addEventListener("click", function () {
+  document.getElementById("feedbackModal").style.display = "block"; // 모달 띄우기
+});
+
+// 좋아요 버튼 클릭 이벤트 추가
+document.getElementById("thumbUpButton").addEventListener("click", function () {
+  document.getElementById("feedbackModal").style.display = "none"; // 모달 닫기
+});
+
+// 싫어요 버튼 클릭 이벤트 추가
+document
+  .getElementById("thumbDownButton")
+  .addEventListener("click", function () {
+    document.getElementById("feedbackModal").style.display = "none"; // 모달 닫기
+  });
